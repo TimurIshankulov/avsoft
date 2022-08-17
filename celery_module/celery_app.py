94ad036d623110b366883af6dd8de0a1b9f7e30a
Timur Ishankulov <tim290592@gmail.com>
2022-08-18 00:30:30 +0300

    Remove unused imports

--- celery_module/celery_app.py
+++ celery_module/celery_app.py
@@ -1,7 +1,6 @@
 import os
 from celery import Celery
-from config import (CELERY_BROKER_URL, CELERY_BROKER_TRANSPORT_OPTIONS, CELERY_ACCEPT_CONTENT,
-                    CELERY_RESULT_BACKEND, CELERY_RESULT_SERIALIZER, CELERY_TASK_SERIALIZER)
+from config import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 
 app = Celery('avsoft',
              broker=CELERY_BROKER_URL,
