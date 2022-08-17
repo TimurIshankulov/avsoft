5d85455a29a27e83e52dc8559a2079824da541ea
Timur Ishankulov <tim290592@gmail.com>
2022-08-17 21:52:53 +0300

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -1,17 +1,19 @@
-DB_NAME = 'avsoft'
-DB_USER = 'root'
-DB_PASSWORD = 'password'
-DB_HOST = 'localhost'
-DB_PORT = '3306'
+import os
+
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_HOST = os.environ['DB_HOST']
+DB_PORT = os.environ['DB_PORT']
 DB_CHARSET = 'utf8mb4'
 DB_CONN_STRING = (f'mysql://{DB_USER}:{DB_PASSWORD}@'
                   f'{DB_HOST}:{DB_PORT}/{DB_NAME}?charset={DB_CHARSET}')
 
-RABBITMQ_USER = 'admin'
-RABBITMQ_PASSWORD = 'password'
-RABBITMQ_HOST = 'localhost'
-RABBITMQ_PORT = '5672'
-RABBITMQ_VHOST = 'vhost'
+RABBITMQ_USER = os.environ['RABBITMQ_USER']
+RABBITMQ_PASSWORD = os.environ['RABBITMQ_PASSWORD']
+RABBITMQ_HOST = os.environ['RABBITMQ_HOST']
+RABBITMQ_PORT = os.environ['RABBITMQ_PORT']
+RABBITMQ_VHOST = os.environ['RABBITMQ_VHOST']
 RABBITMQ_CONN_STRING = (f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@'
                         f'{RABBITMQ_HOST}:{RABBITMQ_PORT}/{RABBITMQ_VHOST}?connection_attempts=20&retry_delay=1')
 PARSING_QUEUE = 'Parsing'
@@ -21,19 +23,15 @@ CELERY_BROKER_URL = (f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@'
                      f'{RABBITMQ_HOST}:{RABBITMQ_PORT}/{RABBITMQ_VHOST}')
 CELERY_RESULT_BACKEND = (f'rpc://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@'
                          f'{RABBITMQ_HOST}:{RABBITMQ_PORT}/{RABBITMQ_VHOST}')
-CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
 
 SMTP_USE_SSL = True
-SMTP_HOST = 'smtp_server'
-SMTP_PORT = 465
-SMTP_HOST_USER = 'user'
-SMTP_HOST_PASSWORD = 'password'
-SMTP_RECEIVER = 'receiver'
+SMTP_HOST = os.environ['SMTP_HOST']
+SMTP_PORT = os.environ['SMTP_PORT']
+SMTP_HOST_USER = os.environ['SMTP_HOST_USER']
+SMTP_HOST_PASSWORD = os.environ['SMTP_HOST_PASSWORD']
+SMTP_RECEIVER = os.environ['SMTP_RECEIVER']
 
-INPUT_PATH = 'data/'
-OUTPUT_PATH = 'output/'
+INPUT_PATH = os.environ['INPUT_PATH']
+OUTPUT_PATH = os.environ['OUTPUT_PATH']
 # Record will be deleted from the database if the word met more than N_TIMES
-N_TIMES = 5
+N_TIMES = os.environ['N_TIMES']
